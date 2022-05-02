4beeecb8a41140249d1e668db8dcb7e0ff0ccf44
Luis <rluis747@gmail.com>
2022-05-02 11:38:57 -0500

    Finished

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,16 +1,20 @@
 const { Router } = require("express");
 const router = Router();
-const {  crearclient, kpiDeClientes, listclientes } = require("../controllers/users.controllers");
+const {
+  crearclient,
+  kpiDeClientes,
+  listclientes,
+} = require("../controllers/users.controllers");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
-router.route("/crearcliente").post(crearclient)
+router.route("/crearcliente").post(crearclient);
 
-router.route('/kpideclientes').get(kpiDeClientes)
+router.route("/kpideclientes").get(kpiDeClientes);
 
-router.route('/listclientes').get(listclientes)
+router.route("/listclientes").get(listclientes);
 
 module.exports = router;
